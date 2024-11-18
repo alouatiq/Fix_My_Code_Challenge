f6ec7b8a57dce6fa893a388006826bd894f8378c
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-18 19:33:21 +0100

    Create 3-user.py

---
+++ 3-user.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+User Model
+"""
+import hashlib
+import uuid
+
+class User:
+    __password = None
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, pwd):
+        if pwd is None or type(pwd) is not str:
+            self.__password = None
+        else:
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
+
+    def is_valid_password(self, pwd):
+        if pwd is None or type(pwd) is not str:
+            return False
+        if self.__password is None:
+            return False
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
+
+if __name__ == '__main__':
+    print("Test User")
+    user_1 = User()
