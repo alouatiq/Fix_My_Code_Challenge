feb009d4420770953921fee5af666313796ab198
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-23 11:31:56 +0100

    Update delete_dnodeint_at_index.c

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,58 +1,49 @@
-#include <stdlib.h>
-#include <stdio.h> /* Added for debug print statements */
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes the node at a given position in a list
+ * delete_dnodeint_at_index - Deletes a node at a specific index from a list
  *
- * @head: The address of the pointer to the first element of the list
- * @index: The index of the node to delete (starting from 0)
+ * @head: A pointer to the pointer to the first element of the list
+ * @index: The index of the node to delete
  *
- * Return: 1 if successful, -1 if it failed
+ * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
-    unsigned int i;
+    dlistint_t *current = *head;
+    unsigned int count = 0;
 
-    /* Check if the list is empty or head is NULL */
-    if (head == NULL || *head == NULL) {
-        printf("Error: List is empty or head is NULL\n"); /* Debug print */
+    if (!head || !*head)
         return (-1);
-    }
 
-    current = *head;
-
-    /* If deleting the first node */
-    if (index == 0) {
-        printf("Deleting node at index 0: %d\n", current->n); /* Debug print */
-        *head = current->next;
-        if (current->next != NULL)
-            current->next->prev = NULL;
-        free(current);
-        return (1);
-    }
-
-    /* Traverse the list to find the node at the given index */
-    for (i = 0; current != NULL && i < index; i++) {
-        printf("Traversing to node %u: %d\n", i, current->n); /* Debug print */
+    /* Traverse to the node at the given index */
+    while (current && count < index)
+    {
         current = current->next;
+        count++;
     }
 
-    /* If the node at the given index doesn't exist */
-    if (current == NULL) {
-        printf("Error: Node at index %u not found\n", index); /* Debug print */
+    /* If index is out of range */
+    if (!current)
         return (-1);
+
+    /* If deleting the head node */
+    if (current == *head)
+    {
+        *head = current->next;
+        if (*head)
+            (*head)->prev = NULL;
+    }
+    else /* Deleting a node other than the head */
+    {
+        if (current->prev)
+            current->prev->next = current->next;
+        if (current->next)
+            current->next->prev = current->prev;
     }
 
-    printf("Deleting node at index %u: %d\n", index, current->n); /* Debug print */
-
-    /* Update the pointers and delete the node */
-    if (current->next != NULL)
-        current->next->prev = current->prev;
-    if (current->prev != NULL)
-        current->prev->next = current->next;
-
+    /* Free the node */
     free(current);
     return (1);
 }
