fd26778a25e6731e2ddaaedef1ca828166a35399
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-23 12:14:08 +0100

    Update delete_dnodeint_at_index.c

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -12,31 +12,29 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
     dlistint_t *current = *head;
-    unsigned int i = 0;
 
     if (!head || !*head) /* Handle empty list */
         return (-1);
 
     /* Traverse to the node at the given index */
-    while (current && i < index)
-    {
+    for (unsigned int i = 0; current && i < index; i++)
         current = current->next;
-        i++;
-    }
 
     if (!current) /* Index out of range */
         return (-1);
 
-    /* Update pointers to remove the node */
-    if (current->prev) /* If not deleting the head node */
+    /* Update links for the previous node */
+    if (current->prev)
         current->prev->next = current->next;
-    else /* Deleting the head node */
+    else /* If deleting the head node */
         *head = current->next;
 
-    if (current->next) /* If not deleting the tail node */
+    /* Update links for the next node */
+    if (current->next)
         current->next->prev = current->prev;
 
+    /* Free the node */
     free(current);
+
     return (1);
 }
-
